424f56d929d2ebba12c85ceaa98ec7cce00678f0
Andrei Balan <andrei.balan98@gmail.com>
2025-06-25 13:09:57 +0300

    db actualizata

--- database/drop-tables.sql
+++ database/drop-tables.sql
@@ -3,11 +3,14 @@
 -- =========================
 
 -- Drop tabelele in ordine inversa pentru a respecta foreign key constraints
+DROP TABLE IF EXISTS likes CASCADE;
+DROP TABLE IF EXISTS ownership CASCADE;
 DROP TABLE IF EXISTS imagini CASCADE;
 DROP TABLE IF EXISTS apartamente CASCADE;
 DROP TABLE IF EXISTS casee CASCADE;
 DROP TABLE IF EXISTS terenuri CASCADE;
 DROP TABLE IF EXISTS spatii_comerciale CASCADE;
 DROP TABLE IF EXISTS anunturi CASCADE;
+DROP TABLE IF EXISTS localitati CASCADE;
+DROP TABLE IF EXISTS orase CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS ownership CASCADE;
